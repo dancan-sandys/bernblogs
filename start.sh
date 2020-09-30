febcff406eb70e47a0d88c51e9e30c2d27ed3d08
Bernice2001 <wangechibanim@gmail.com>
2020-09-30 21:58:15 +0300

    created a shell

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://mainas:charity@localhost/blog'
+export MAIL_USERNAME='wangechibanim@gmail.com'
+export MAIL_PASSWORD='2001MAINABERNICE'
+export SECRET_KEY='secret'
+python3.8 manage.py server
